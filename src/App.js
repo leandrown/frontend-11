8278b3aac0f28bb121c06bec8cd75cb0a63a3f36
Leandro Vieira <leandrown@outlook.com>
2020-03-25 12:25:10 -0300

    Conceitos do React realizado
    - Componente
    - JSX
    - Propriedades
    - Estado
    - Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  // useState retorna um Array com duas posições
+  // Array[valor, funcaoDeAtualizacao]
+
+  // valor => valor da variavel
+  // funcaoDeAtualizacao => funcao de atualizacao do valor
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
