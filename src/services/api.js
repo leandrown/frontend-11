7d2f4da1aacbe605d8262d28bd32bc877fd7a1cb
Leandro Vieira <leandrown@outlook.com>
2020-03-26 11:16:10 -0300

    Cadastro de ONGs, Listagem e Cadastro de Casos finalizado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseUrl: 'http://localhost:3333'
+   baseUrl: 'http://localhost:3333' // TODO React com CORS: corrigir bug
 });
 
 export default api;
